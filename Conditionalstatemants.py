181addd4ccd4d27ef8ef1e2b4a61df809684dc12
chrismeso <chrismeso695@gamil.com>
2024-06-13 10:38:53 +0300

    initial commit

--- Conditionalstatemants.py
+++ Conditionalstatemants.py
@@ -22,8 +22,26 @@ else:
 
 # A python prgram that checks if a number is even or odd
 
-num = int (input ("Enter any number to test whether it is odd or even:"))
-if num % 2 == 0:
+num = 78
+if num  == 0:
+    print(num, "is neutral number")
+
+elif num % 2 == 0:
     print(num, "is an even number")
 else:
     print(num, "is an odd number")
+
+# A python program that returns the area of a rectangle
+# A = L * W
+Length = 20
+Width = 5
+area = Length * Width
+print("The area is", area)
+
+# A python prgram that determines the area of a trapezium
+# A = 0.5 (a+b)*h
+a = 10
+b = 20
+h = 5
+area1 = 0.5*(10+20)*5
+print("The area is", area1)
---
+++ areaofacircle.py
@@ -0,0 +1,7 @@
+# A program to return the area of a circle
+# A = PI * R * R
+
+radius = int(input("Enter the radius of the circle: "))
+
+area = 3.14 * radius * radius
+print("The area of the circle is:", area)
